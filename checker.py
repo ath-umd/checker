b375b24add3618e9cd2584a4be848297d320471c
ath-umd <athall@umd.edu>
2021-10-07 10:00:09 -0400

    Update checker.py
    Adds volume, runs much slower

--- checker.py
+++ checker.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import pymatgen as pmg
 import numpy as np
-dir = '/lustre/athall/LLZO/cubic/Ta/bulk_mod/vol_relax/rand_100'
+dir = '/lustre/athall/LLZO/cubic/Ta/bulk_mod/Li_then_Ta'
 
 energies = []
 
@@ -13,14 +13,14 @@ for dirname in next(os.walk('.'))[1]:
 #    os.system('tail -1 '+dir+'/%s/relax/vaspinput/OUTCAR' % dirname)
 ### Saves the toten from vasprun ###
     run = pmg.io.vasp.Vasprun('%s/relax/vaspinput/vasprun.xml' % dirname)
-    energies.append([dirname,run.final_energy])
-
+    struct = pmg.Structure.from_file('%s/relax/vaspinput/vasprun.xml' % dirname)
+    energies.append([dirname,struct.volume,run.final_energy])
 np.save('energies.npy',energies)
 
 energ = []
 
 for i in np.load('energies.npy'):
-    i[1] = i[1].replace(' eV', '')
-    energ.append([i[0],float(i[1])])
+    i[2] = i[2].replace(' eV', '')
+    energ.append([i[0],i[1],float(i[2])])
 
 np.save('energies.npy',energ)
